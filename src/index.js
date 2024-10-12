fa1b015b15a2e97e70c3788ff12d892d0839e92c
ChristosKalog <christoskalogridakis@gmail.com>
2024-10-12 12:16:29 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,25 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import process from 'process';
 
-window.process = {
-  env: {
-    NODE_ENV: 'development' // or 'production' based on your environment
-  }
-};
-window.process = process;
-
-
-
-if (typeof process !== 'undefined' && process.versions && process.versions.node) {
-  // Code that runs only in Node.js
-  console.log('Running in Node.js');
-} else {
-  // Code that runs in the browser
-  console.log('Running in the browser');
+if (typeof window !== 'undefined') {
+  window.process = {
+    env: {
+      NODE_ENV: 'development' // or 'production' based on your environment
+    }
+  };
 }
 
+// Optionally log the environment
+if (typeof process !== 'undefined' && process.versions && process.versions.node) {
+  console.log('Running in Node.js');
+} else {
+  console.log('Running in the browser');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -29,8 +25,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
