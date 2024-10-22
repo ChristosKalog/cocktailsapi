3852905cb2029045eebe8ca65821fb8ea9811801
ChristosKalog <christoskalogridakis@gmail.com>
2024-10-22 17:51:16 +0300

    1
    1

--- cocktailFront/src/data/startServer.js
+++ cocktailFront/src/data/startServer.js
@@ -1,1 +1,2 @@
-// npx json-server --watch cocktailFront/src/data/db.json --port 5001
+// // npx json-server --watch cocktailFront/src/data/db.json --port 5001
+// npx json-server --watch cocktailFront/src/data/db.json --port 5001 --host 0.0.0.0
