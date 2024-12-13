559bf8c1ac53a379656c48fcc40bfbd7b52cc6c3
ChristosKalog <christoskalogridakis@gmail.com>
2024-12-13 12:03:32 +0200

    okay

--- cocktailFront/src/services/authService.js
+++ cocktailFront/src/services/authService.js
@@ -7,12 +7,14 @@ const login = async (username, password) => {
     const users = await response.json();
 
     // Find the user based on the username and password
-    const user = users.find(user => user.username === username && user.password === password);
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
 
     if (user) {
       return { ...user, isAuthenticated: true }; // Return user data with authentication status
     } else {
-      throw new Error('Invalid credentials'); // Handle invalid login
+      throw new Error("Invalid credentials"); // Handle invalid login
     }
   } catch (error) {
     console.error("Login error:", error);
@@ -24,15 +26,15 @@ const login = async (username, password) => {
 const register = async (newUser) => {
   try {
     const response = await fetch(API_URL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newUser),
     });
 
     if (!response.ok) {
-      throw new Error('Failed to register user');
+      throw new Error("Failed to register user");
     }
 
     const createdUser = await response.json();
